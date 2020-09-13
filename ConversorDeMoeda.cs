eeb929fa732f4c4d1ce68c1f950079e843a57ace
Fábio Martins <66891924+fmadafonseca@users.noreply.github.com>
2020-09-13 17:18:52 -0300

    Add files via upload

---
+++ ConversorDeMoeda.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConversorDolar
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+
+
+        public static double DolarParaReal(double quantia, double cotacao) 
+        {
+            double total = quantia * cotacao;
+            return total + total * Iof / 100;
+        }
+
+
+
+    }
+}
